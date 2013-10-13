a32932784fa294e588742aa4d1ab38f2e9a07078
S.Penguin <Symulation12@gmail.com>
2013-10-12 17:41:49 -0700

    Events!

--- PenguinBindings.lua
+++ PenguinBindings.lua
@@ -1,4 +1,41 @@
-SLASH_PENGUINBINDINGS1 = '/penguinbindings'
-function SlashCmdList.PENGUINBINDINGS(msg,editbox)
-	print("Hello, world")
+--Maintain Separation between game hotkeys and mine
+--PenguinBindVars 
+local loaded = false
+local function pPrint(text)
+	DEFAULT_CHAT_FRAME:AddMessage(text)
 end
+
+local penguinFrame = CreateFrame("Frame")
+
+
+
+local function onEvent(self,event,...)
+	if event == "ADDON_LOADED" then
+		if not loaded then
+			pPrint("I was loaded!")
+		end
+		loaded = true
+	elseif event == "ACTIVE_TALENT_GROUP_CHANGED" then
+		pPrint("You changed talents!")
+	elseif event == "UPDATE_SHAPESHIFT_FORM" then
+	end
+end
+
+
+SLASH_PENGUINBINDINGS1 = "/pb"
+SlashCmdList["PENGUINBINDINGS"] = function(argString,editbox)
+	local args = {}
+	for x in argString:gmatch("%S+") do
+		table.insert(args,x)
+	end
+	if args[1] == "set" then
+		SetBindingSpell("Q","Penance")
+	else
+		pPrint(GetBindingAction("X"))
+	end
+end
+
+penguinFrame:RegisterEvent("ADDON_LOADED")
+penguinFrame:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
+penguinFrame:RegisterEvent("UPDATE_SHAPESHIFT_FORM")
+penguinFrame:SetScript("OnEvent",onEvent)
